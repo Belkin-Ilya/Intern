3d7adff3d5e29dff136f0ae8a150496a61e5700f
feelthevoid <belkin888@inbox.ru>
2022-10-21 20:45:21 +0300

    Initial Commit

---
+++ Avito_internship_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avito_internship_task
+//
+//  Created by Илья Белкин on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
